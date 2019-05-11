bbd8a4255565260c564971fa499e7e28de3c5470
DDDenisD <49097406+DDDenisD@users.noreply.github.com>
2019-05-11 19:04:04 +0300

    Add files via upload

---
+++ sketch_may11c/sketch_may11c.ino
@@ -0,0 +1,35 @@
+String R = "";
+char* IN[256];
+
+void setup() {
+  Serial.begin(115200);
+
+}
+
+void loop() {
+
+  int i = 0;
+  int C = 255;
+  for(C; C>-1; C--){
+    IN[C] = "";
+  }
+  while(Serial.available() == 0);
+  while(Serial.available() > 0) {
+    IN[i] = (char*)Serial.read();
+    i++;
+  }
+  i = 0;
+  while(IN[i] != ""){
+    R += (char*)IN[i];
+    i++;
+  }
+  Serial.println(R);
+
+  //R += (char)Serial.read();
+  //if(Serial.available() == 0){
+  //  Serial.println(R);
+  //  R = "";
+  //}
+}
+
+
