c28d620e1aacd6301085cf8c59c77db9ac7e4b20
DDDenisD <49097406+DDDenisD@users.noreply.github.com>
2019-05-24 14:03:51 -0700

    Add files via upload

---
+++ Smoke_On_The_Water.ino
@@ -0,0 +1,77 @@
+void setup(){
+  pinMode(13, OUTPUT);
+}
+
+void loop(){
+  tone(13, 147);
+  delay(128);
+  
+  noTone(13);
+  delay(128);
+  
+  tone(13, 175);
+  delay(128);
+  
+  noTone(13);
+  delay(128);
+  
+  tone(13, 196);
+  delay(256);
+  
+  noTone(13);
+  delay(128);
+  
+  tone(13, 147);
+  delay(128);
+  
+  
+  noTone(13);
+  delay(128);
+  
+  tone(13, 175);
+  delay(128);
+  
+  noTone(13);
+  delay(128);
+  
+  tone(13, 220);
+  delay(128);
+  
+  tone(13, 196);
+  delay(384);
+  
+  noTone(13);
+  delay(128);
+  
+  
+  tone(13, 147);
+  delay(128);
+  
+  noTone(13);
+  delay(128);
+  
+  tone(13, 175);
+  delay(128);
+  
+  noTone(13);
+  delay(128);
+  
+  tone(13, 196);
+  delay(256);
+  
+  noTone(13);
+  delay(128);
+  
+  tone(13, 175);
+  delay(128);
+  
+  
+  noTone(13);
+  delay(128);
+  
+  tone(13, 147);
+  delay(640);
+  
+  noTone(13);
+  delay(256);
+}
